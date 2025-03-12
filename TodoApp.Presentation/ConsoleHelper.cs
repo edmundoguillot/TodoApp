231a1193161817214ec6db6231b07f12216ead34
Edmundo Guillot <edmundo.guillot@thetrainline.com>
2025-03-12 17:23:10 +0000

    error message fix and removed 'System.'

--- TodoApp.Presentation/ConsoleHelper.cs
+++ TodoApp.Presentation/ConsoleHelper.cs
@@ -5,16 +5,16 @@ public static class ConsoleHelper
     public static string GetSelection(string prompt, List<string> validOptions, string? errorMessage = null)
     {
         Console.WriteLine(prompt);
-        var userInput = System.Console.ReadKey();
+        var userInput = Console.ReadKey();
 
-        while (!validOptions.Contains(userInput.KeyChar.ToString().ToLower()))
+        while (!validOptions.Contains(userInput.KeyChar.ToString(), StringComparer.CurrentCultureIgnoreCase))
         {
-            if (errorMessage != null)
-                System.Console.WriteLine($"\n{errorMessage}");
+            if (errorMessage == null)
+                Console.WriteLine($"\n'{userInput.KeyChar}' is not a valid value, please choose from 'y' or 'n'");
             else
-                System.Console.WriteLine();
+                Console.WriteLine($"\n{errorMessage}");
             
-            userInput = System.Console.ReadKey();
+            userInput = Console.ReadKey();
         }
         
         return userInput.KeyChar.ToString();
