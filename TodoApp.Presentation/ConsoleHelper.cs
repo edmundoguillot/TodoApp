7b116d0d2f0eaa4962f546e91ef17593bf4dd3fd
Edmundo Guillot <edmundo.guillot@thetrainline.com>
2025-03-13 09:37:12 +0000

    dynamic error msg

--- TodoApp.Presentation/ConsoleHelper.cs
+++ TodoApp.Presentation/ConsoleHelper.cs
@@ -5,18 +5,20 @@ public static class ConsoleHelper
     public static string GetSelection(string prompt, List<string> validOptions, string? errorMessage = null)
     {
         Console.WriteLine(prompt);
-        var userInput = Console.ReadKey();
+        var userInput = Console.ReadLine();
 
-        while (!validOptions.Contains(userInput.KeyChar.ToString(), StringComparer.CurrentCultureIgnoreCase))
+        while (!validOptions.Contains(userInput, StringComparer.CurrentCultureIgnoreCase))
         {
-            if (errorMessage == null)
-                Console.WriteLine($"\n'{userInput.KeyChar}' is not a valid value, please choose from 'y' or 'n'");
+            // the first condition is to handle the case where the user just press enter
+            if (string.IsNullOrEmpty(userInput))
+                Console.WriteLine($"Please enter a value. Choose from: {string.Join(", ", validOptions)}");
+            else if (errorMessage == null)
+                Console.WriteLine($"'{userInput}' is not a valid value, please choose from {string.Join(", ", validOptions)}");
             else
-                Console.WriteLine($"\n{errorMessage}");
+                Console.WriteLine($"{errorMessage}");
             
-            userInput = Console.ReadKey();
+            userInput = Console.ReadLine();
         }
-        
-        return userInput.KeyChar.ToString();
+        return userInput;
     }
 }
