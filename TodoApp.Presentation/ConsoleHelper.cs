415897783c88aaef3543fe4612f0002fb4fb7105
Edmundo Guillot <edmundo.guillot@thetrainline.com>
2025-04-25 12:07:39 +0100

    task 6

--- TodoApp.Presentation/ConsoleHelper.cs
+++ TodoApp.Presentation/ConsoleHelper.cs
@@ -4,16 +4,16 @@ public static class ConsoleHelper
 {
     public static string GetSelection(string prompt, List<string> validOptions, string? errorMessage = null)
     {
-        Console.WriteLine(prompt);
-        var userInput = Console.ReadLine();
-
-        while (string.IsNullOrWhiteSpace(userInput) || !validOptions.Contains(userInput, StringComparer.CurrentCultureIgnoreCase))
+        string? ValidateFunction(string userInput)
         {
-            Print(errorMessage ?? $"'{userInput ?? string.Empty}' is not a valid value, please choose from {string.Join(", ", validOptions)}");
-            userInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userInput) || !validOptions.Contains(userInput, StringComparer.CurrentCultureIgnoreCase))
+            {
+                return errorMessage ?? $"'{userInput}' is not a valid value, please choose from {string.Join(", ", validOptions)}";
+            }
+            return null;
         }
-        
-        return userInput;
+
+        return InputHandling.GetInput<string>(prompt, ValidateFunction);
     }
 
     public static void Print(string message, ConsoleColor color = ConsoleColor.Gray, bool newLine = true)
